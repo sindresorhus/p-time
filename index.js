6f0c9af7c6e5786efdcb894ddf228dc2646de025
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-12 15:28:22 +0200

    add TODO

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 module.exports = fn => {
 	const ret = function () {
 		const start = Date.now();
+		// TODO: use rest/spread when Node.js 6 is targeted
 		const promise = fn.apply(null, arguments);
 
 		const retPromise = promise.then(res => {
